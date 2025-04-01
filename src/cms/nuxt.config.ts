6d98abb341c60bf00bd0225f797dceab08215895
Le Nguyen <nguyenthile.work@gmail.com>
2025-04-01 23:12:31 +0700

    add: some configs

--- src/cms/nuxt.config.ts
+++ src/cms/nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   css: ["~/assets/css/main.css", "~/assets/scss/main.scss"],
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss"],
+  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss", "@nuxt/ui"],
+  compatibilityDate: "2025-03-31",
 });
