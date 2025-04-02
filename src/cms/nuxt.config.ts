636b8f72c9ef8981de73595279b892ec6a01c7ab
Le Nguyen <nguyenthile.work@gmail.com>
2025-04-02 23:27:00 +0700

    add: resolve install error

--- src/cms/nuxt.config.ts
+++ src/cms/nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   css: ["~/assets/css/main.css", "~/assets/scss/main.scss"],
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss", "@nuxt/ui"],
   compatibilityDate: "2025-03-31",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
