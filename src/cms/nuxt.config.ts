78198e0823a9aa444cebb22b4ebc23804a1953e1
Le Nguyen <nguyenthile.work@gmail.com>
2025-04-09 23:19:44 +0700

    add: config tailwind

--- src/cms/nuxt.config.ts
+++ src/cms/nuxt.config.ts
@@ -1,13 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   css: ["~/assets/css/main.css", "~/assets/scss/main.scss"],
+  ui: {
+    prefix: "Nuxt",
+  },
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss", "@nuxt/ui"],
+  modules: ["@nuxt/eslint", "@nuxt/ui"],
   compatibilityDate: "2025-03-31",
   vite: {
     plugins: [tailwindcss()],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // additionalData: '@use "~/assets/_variables.scss" as *;',
+        },
+      },
+    },
   },
 });
---
+++ src/cms/pages/settings/members.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <UDashboardPanel>
+    <template #header>
+      <UDashboardNavbar />
+    </template>
+  </UDashboardPanel>
+</template>
