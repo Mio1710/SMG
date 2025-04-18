4e5f673e312ad256597150007f927b0891f8620a
Le Nguyen <nguyenthile.work@gmail.com>
2025-04-18 22:57:37 +0700

    add: tool bar

--- src/cms/components/layouts/NavigationMenu.vue
+++ src/cms/components/layouts/NavigationMenu.vue
@@ -1,44 +1,35 @@
 <script setup lang="ts">
-const rail = ref(true);
-const drawer = ref(true);
+const props = defineProps({
+  drawer: {
+    type: Boolean,
+    default: true,
+  },
+});
 </script>
 
 <template>
-  <v-card>
-    <v-navigation-drawer
-      v-model="drawer"
-      :rail="rail"
-      permanent
-      @click="rail = false"
-    >
-      <div class="flex items-center">
-        <v-img
-          src="public/logo.svg"
-          height="60px"
-          width="60px"
-          alt="Medimio"
-          @click.stop="rail = !rail"
-        />
-        <span class="w-full ml-2 text-2xl font-bold text-[#45c74c]"
-          >Miodium</span
-        >
-      </div>
+  <v-navigation-drawer :rail="props.drawer" permanent>
+    <div class="flex items-center">
+      <v-img src="public/logo.svg" height="48px" width="48px" alt="Medimio" />
+      <span class="w-full ml-2 text-2xl font-bold text-[#45c74c]">
+        Miodium</span
+      >
+    </div>
 
-      <v-divider />
+    <v-divider />
 
-      <v-list density="compact" nav>
-        <v-list-item prepend-icon="mdi-home-city" title="Home" value="home" />
-        <v-list-item
-          prepend-icon="mdi-account"
-          title="My Account"
-          value="account"
-        />
-        <v-list-item
-          prepend-icon="mdi-account-group-outline"
-          title="Users"
-          value="users"
-        />
-      </v-list>
-    </v-navigation-drawer>
-  </v-card>
+    <v-list density="compact" nav>
+      <v-list-item prepend-icon="mdi-home-city" title="Home" value="home" />
+      <v-list-item
+        prepend-icon="mdi-account"
+        title="My Account"
+        value="account"
+      />
+      <v-list-item
+        prepend-icon="mdi-account-group-outline"
+        title="Users"
+        value="users"
+      />
+    </v-list>
+  </v-navigation-drawer>
 </template>
--- src/cms/layouts/default.vue
+++ src/cms/layouts/default.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
+import AppBar from "~/components/layouts/AppBar.vue";
 import NavigationMenu from "~/components/layouts/NavigationMenu.vue";
+
+const drawer = ref(true);
 </script>
 
 <template>
   <v-layout>
-    <NavigationMenu class="p-3" />
+    <NavigationMenu :drawer class="p-3" />
+    <AppBar class="p-2" @changeDrawer="drawer = !drawer" />
+
     <v-main>
       <slot />
     </v-main>
