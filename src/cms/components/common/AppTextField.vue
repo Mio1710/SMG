dd9540cbd4aeb64783c9fe205317ff8b94cf1ed6
Le Nguyen <nguyenthile.work@gmail.com>
2025-04-20 22:37:11 +0700

    add: login ui and remove vee validate

---
+++ src/cms/components/common/AppTextField.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const value = defineModel({
+  type: String,
+  default: '',
+})
+</script>
+
+<template>
+  <v-text-field
+    v-model="value"
+    color="primary"
+    label="First name"
+    variant="underlined"
+  />
+</template>
