15fed21dd483c0422c2b8c7b06776e9893fc14c1
Le Nguyen <nguyenthile.work@gmail.com>
2025-06-12 23:49:15 +0700

    add: define schema

--- src/backend/src/routes/swagger.ts
+++ src/backend/src/routes/swagger.ts
@@ -2,6 +2,32 @@ export const swaggerDocument = {
   swagger: "2.0",
   info: { version: "1.0.0", title: "Example API" },
   paths: {
-    "/user": { get: { responses: { 200: { description: "all users" } } } },
+    "/user": {
+      get: {
+        responses: {
+          200: {
+            description: "all users",
+          },
+        },
+      },
+    },
+    "/auth/login": {
+      post: {
+        summary: "Login user",
+        requestBody: {
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  email: { type: "string", format: "email" },
+                  password: { type: "string", minLength: 6 },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
   },
 };
